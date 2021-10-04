73735bb7d428f71e0fa30aceadde262a3fae78c5
sambrosio17 <ambrosio.s@outlook.it>
2021-10-04 20:31:37 +0200

    create una classe contenenti utte le stringhe costanti

---
+++ src/Utils/StringCostants.java
@@ -0,0 +1,9 @@
+package Utils;
+
+public class StringCostants {
+
+    public static final String ADD="ADD";
+    public static final String MODIFY="MODIFY";
+    public static final String CHANGE="CHANGE";
+    public static final String REMOVE="REMOVE";
+}
